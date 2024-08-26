3237ca099ccaa97b7dbec35472b66b22972e26e0
hetmith01 <het.hanc.14@gmail.com>
2024-08-26 15:50:24 -0500

    insercion de datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,22 @@
+INSERT INTO fabricantes(id, nombre) VALUES(1, "Asus");
+INSERT INTO fabricantes(id, nombre) VALUES(2, "Lenovo");
+INSERT INTO fabricantes(id, nombre) VALUES(3, "Hewlett-Packard");
+INSERT INTO fabricantes(id, nombre) VALUES(4, "Samsung");
+INSERT INTO fabricantes(id, nombre) VALUES(5, "Seagate");
+INSERT INTO fabricantes(id, nombre) VALUES(6, "Crucial");
+INSERT INTO fabricantes(id, nombre) VALUES(7, "Gigabyte");
+INSERT INTO fabricantes(id, nombre) VALUES(8, "Huawei");
+INSERT INTO fabricantes(id, nombre) VALUES(9, "Xiaomi");
+
+
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(1, "Disco duro", 86.99, 5);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(2, "Memoria RAM 8GB", 120, 6);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(3, "Disco SSD 1T", 150.99, 4);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(4, "Geforce GTX 1050Ti", 185, 7);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(5, "Geforce GTX 1050Ti xtreme", 755, 6);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(6, "Monitor 24 LED Full HD", 202, 1);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(7, "Monitor 27 LED Full HD", 245.99, 1);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(8, "Portatil Yoga 520", 559, 2);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(9, "Portatil otro", 444, 2);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(10, "Impresora cara", 59.99, 3);
+INSERT INTO productos(id, nombre_producto, precio, fabricante_id) VALUES(11, "Impresora más cara todavía", 180, 3);
