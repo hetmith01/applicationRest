ecbee39455d0dbf527f5f8019f481140f38e24be
hetmith01 <het.hanc.14@gmail.com>
2024-08-28 02:47:46 -0500

    controller crud fabricante implementado

--- src/main/java/com/application/rest/SpringBootRest/controller/FabricanteController.java
+++ src/main/java/com/application/rest/SpringBootRest/controller/FabricanteController.java
@@ -6,11 +6,11 @@ import com.application.rest.SpringBootRest.entities.Fabricante;
 import com.application.rest.SpringBootRest.service.IFabricanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -36,4 +36,68 @@ public class FabricanteController {
         }
         return  ResponseEntity.notFound().build();
     }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<FabricanteDTO> fabricanteList = fabricanteService.findAll()
+                .stream()
+                .map(fabricante -> FabricanteDTO.builder()
+                        .id(fabricante.getId())
+                        .nombre(fabricante.getNombre())
+                        .productoList(fabricante.getProductoList())
+                        .build())
+                .toList();
+        return ResponseEntity.ok(fabricanteList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody FabricanteDTO fabricanteDTO)throws URISyntaxException {
+
+        if(fabricanteDTO.getNombre().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        fabricanteService.save(Fabricante.builder()
+                .nombre(fabricanteDTO.getNombre())
+                .build());
+
+        return ResponseEntity.created(new URI("/api/fabricante/save")).build();
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateFabricante(@PathVariable Long id, @RequestBody FabricanteDTO fabricanteDTO){
+
+        Optional<Fabricante> fabricanteOptional = fabricanteService.findById(id);
+
+        if(fabricanteOptional.isPresent()){
+            Fabricante fabricante = fabricanteOptional.get();
+            fabricante.setNombre(fabricanteDTO.getNombre());
+            fabricanteService.save(fabricante);
+            return ResponseEntity.ok("Registro actualizado!!!");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        if(id != null){
+            fabricanteService.deleteById(id);
+            return ResponseEntity.ok("Registro eliminado!!!");
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
