daf611d09401eb1c687502497bed9b900b67bf4f
hetmith01 <het.hanc.14@gmail.com>
2024-08-30 02:44:38 -0500

    controller producto, terminado

--- src/main/java/com/application/rest/SpringBootRest/controller/ProductoController.java
+++ src/main/java/com/application/rest/SpringBootRest/controller/ProductoController.java
@@ -6,11 +6,11 @@ import com.application.rest.SpringBootRest.entities.Producto;
 import com.application.rest.SpringBootRest.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -38,4 +38,77 @@ public class ProductoController {
         }
        return ResponseEntity.notFound().build();
     }
+
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<ProductoDTO> productoDTOList = productoService.findAll()
+                .stream()
+                .map(producto -> ProductoDTO.builder()
+                        .id(producto.getId())
+                        .nombre(producto.getNombre())
+                        .precio(producto.getPrecio())
+                        .fabricante(producto.getFabricante())
+                        .build())
+                .toList();
+        return ResponseEntity.ok(productoDTOList);
+    }
+
+    @PostMapping("/save")
+    public  ResponseEntity<?> save(@RequestBody ProductoDTO productoDTO) throws URISyntaxException {
+
+        if(productoDTO.getNombre().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        productoService.save(Producto.builder()
+                .nombre(productoDTO.getNombre())
+                .precio(productoDTO.getPrecio())
+                .fabricante(productoDTO.getFabricante())
+                .build());
+
+        return ResponseEntity.created(new URI("/api/producto/save")).build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ProductoDTO productoDTO){
+
+        Optional<Producto> productoOptional = productoService.findById(id);
+
+        if(productoOptional.isPresent()){
+            Producto producto = productoOptional.get();
+            producto.setNombre(productoDTO.getNombre());
+            producto.setPrecio(productoDTO.getPrecio());
+            producto.setFabricante(productoDTO.getFabricante());
+            return ResponseEntity.ok("Registro actualizado!!!");
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+
+        if(id != null){
+            productoService.deleteById(id);
+            return ResponseEntity.ok("Registro eliminado!!!");
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
